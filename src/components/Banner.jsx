c450999037b4e31721b973c4949d81985b835af9
Munna-Scripts <munnascriptz@gmail.com>
2025-09-14 17:56:41 +0600

    Navbar Complete & Banner 80% Complete

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,56 @@ import { Link } from 'react-router'
 const Banner = () => {
   return (
     <>
-        <section id='Banner' className='mt-[40px] h-[863px]'>
+        <section id='Banner' className='h-[863px]'>
             <div className="container">
-                <div id="Banner-Row" className='flex lg:flex-row flex-col items-center justify-between'>
+                <div id="Banner-Row" className='pt-[184px]'>
+                    {/* ---------------Header Text---------------- */}
+                    <div>
+                        <h1 className='text-white font-semibold text-[60px] text-center'>Find Next Place To Visit</h1>
+                        <p className='text-white text-base text-center mt-1.5'>Discover amazing places at exclusive deals</p>
+                    </div>
+                    {/* ---------------Banner Navigation---------------- */}
+                    <div className='mt-[60px]'>
+                        {/* ----------Links---------- */}
+                        <div>
+                            <ul className='flex items-center gap-[34px] text-white justify-center'>
+                                <li>
+                                    <Link>Hotel</Link>
+                                </li>
+                                <li>
+                                    <Link>Tour</Link>
+                                </li>
+                                <li>
+                                    <Link>Activity</Link>
+                                </li>
+                                <li>
+                                    <Link>Holiday Rentals </Link>
+                                </li>
+                                <li>
+                                    <Link>Car</Link>
+                                </li>
+                                <li>
+                                    <Link>Cruise</Link>
+                                </li>
+                                <li>
+                                    <Link>Flights</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        {/* --------------Box------------ */}
+                        <div className='w-[900px] h-[80px] mt-8 bg-white mx-auto rounded-full pl-[36px] pt-[18px] pb-[13px] flex items-center'>
+                            <div className='mr-[79px]'>
+                                <h2 className='text-primary font-medium text-[15px]'>Location</h2>
+                                <p className='text-second text-[15px] mt-1'>Where are you going?</p>
+                            </div>
+
+                            <div className='border-l-1 border-[#DADFE6] pl-5'>
+                                <h2 className='text-primary font-medium text-[15px]'>Check in - Check out</h2>
+                                <p className='text-second text-[15px] mt-1'>Wed 2 Mar  -  Fri 11 Apr</p>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,51 @@
 import React from 'react'
 import logo from '../assets/images/logo.svg'
 import { Link } from 'react-router'
+import { IoMdArrowDropdown } from 'react-icons/io'
 const Navbar = () => {
   return (
     <>
         <nav className='py-[20px] px-[30px] lg:block hidden'>
             <div>
                 <div id="Nav-Row" className='flex items-center justify-between'>
-                  {/* -----------------Logo-------------------- */}
-                  <Link to={'/'}><img src={logo} alt="Logo" /></Link>
-                  {/* -----------------Logo-------------------- */}
+                  <div className='flex items-center gap-[50px]'>
+                    {/* -----------------Logo-------------------- */}
+                    <Link to={'/'}><img src={logo} alt="Logo" /></Link>
+                    {/* -----------------Nav Links-------------------- */}
+                    <ul className='flex items-center gap-[62px] text-white font-jost font-medium'>
+                      <li>
+                        <Link className='relative after:absolute after:left-0 after:bottom-0 after:bg-white after:w-0 hover:after:w-full after:h-[1px] after:duration-500 duration-[.3s]'>Home</Link>
+                      </li>
+                      <li className='flex items-center gap-1.5'>
+                        <Link className='relative after:absolute after:left-0 after:bottom-0 after:bg-white after:w-0 hover:after:w-full after:h-[1px] after:duration-500 duration-[.3s]'>Categories</Link>
+                        <IoMdArrowDropdown />
+                      </li>
+                      <li>
+                        <Link className='relative after:absolute after:left-0 after:bottom-0 after:bg-white after:w-0 hover:after:w-full after:h-[1px] after:duration-500 duration-[.3s]'>Blog</Link>
+                      </li>
+                      <li>
+                        <Link className='relative after:absolute after:left-0 after:bottom-0 after:bg-white after:w-0 hover:after:w-full after:h-[1px] after:duration-500 duration-[.3s]'>Contact</Link>
+                      </li>
+                    </ul>
+                  </div>
+                  {/* ------------------2nd Part--------------- */}
+                  <div className='w-[608px] flex items-center justify-between'>
+                    <div className='flex items-center gap-4'>
+                      <div className='text-white cursor-pointer flex items-center gap-1.5'>
+                        <p>USD</p>
+                        <IoMdArrowDropdown />
+                      </div>
+                      <div className='w-[2px] h-[20px] bg-white opacity-20'>
+
+                      </div>
+                    </div>
+
+                    <div>
+                      <Link to={'/'} className='text-white border-1 border-white rounded-[4px] py-[15px] px-[30px] hover:bg-brand duration-300 hover:border-transparent'>
+                        Sign In / Register
+                      </Link>
+                    </div>
+                  </div>
                 </div>
             </div>
         </nav>
